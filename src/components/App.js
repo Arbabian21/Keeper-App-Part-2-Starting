264ad7db8010fe4f214e1036d0d273ee7dbbde8b
Arbabian21 <arifarbab21@gmail.com>
2024-09-07 13:32:04 -0400

    Mapped notes array

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Note />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			{notes.map((entry) => (
+				<Note key={entry.key} title={entry.title} message={entry.content} />
+			))}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
